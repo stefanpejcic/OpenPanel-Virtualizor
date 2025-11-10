1b3dbca57bc63ce0dfaee26025c285f2e7e2e70c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-10 23:59:18 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
+set -e
 
-# check if virtualizor
+readonly GITHUB_REPO="https://github.com/stefanpejcic/OpenPanel-Virtualizor"
 
+#1. check requirements
+if [[ $EUID -ne 0 ]]; then
+    echo "[✖] This script must be run as root!"
+    exit 1
+fi
 
-# clone script
+if command -v virtualizor >/dev/null 2>&1; then
+    echo "[✔] Virtualizor detected - proceeding with the install.."
+else
+    echo "[✖] Virtualizor binary not found! Is this a Virtualizor master server?"
+    exit 1
+fi
+
+#2. set cleanup
+cleanup() {
+    echo
+    echo "Cleaning up..."
+    rm -rf "/tmp/OpenPanel-Virtualizor"
+}
+
+trap cleanup EXIT INT TERM
+
+#3. clone feom github
+echo
+echo "Clonning $GITHUB_REPO"
 cd /tmp
-git clone https://github.com/stefanpejcic/OpenPanel-Virtualizor
+git clone "$GITHUB_REPO"
 
-# setup according to https://www.virtualizor.com/docs/admin/adding-custom-control-panel/
-cd /tmp/OpenPanel-Virtualizor
+#4. https://youtu.be/tw429JGL5zo
+echo
+echo "Setup according to https://www.virtualizor.com/docs/admin/adding-custom-control-panel/"
 cp virt_openpanel.sh /usr/local/virtualizor/hooks/virt_openpanel.sh
 cp virt_openpanel.png /usr/local/virtualizor/enduser/themes/default/images/virt_openpanel.png
 cp openpanel_supported_os.json /usr/local/virtualizor/openpanel_supported_os.json
 
-#cleanul
-cd ../
-rm -rf /tmp/OpenPanel-Virtualizor
+#5. that's it
+echo "Done"
+exit 0
