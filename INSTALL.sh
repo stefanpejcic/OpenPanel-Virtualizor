c38d3984828ca00aa1cf3f05bb18d7858cdc4847
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-10 23:42:33 +0100

    Paths per docs

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-:'
-https://www.virtualizor.com/docs/admin/adding-custom-control-panel/
-'
-
+# https://www.virtualizor.com/docs/admin/adding-custom-control-panel/
 
 cd /tmp
 git clone https://github.com/stefanpejcic/OpenPanel-Virtualizor
+
 cd /tmp/OpenPanel-Virtualizor
-cp virt_openpanel.sh /usr/local/virtualizor/hook/
-cp virt_openpanel.png /usr/local/virtualizor/enduser/themes/default/images/
-cp openpanel_supported_os.json /usr/local/virtualizor/
+cp virt_openpanel.sh /usr/local/virtualizor/hooks/virt_openpanel.sh
+cp virt_openpanel.png /usr/local/virtualizor/enduser/themes/default/images/virt_openpanel.png
+cp openpanel_supported_os.json /usr/local/virtualizor/openpanel_supported_os.json
+
 cd ../
 rm -rf /tmp/OpenPanel-Virtualizor
