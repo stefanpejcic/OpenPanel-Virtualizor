339c828c75a2b6113d59815403550300becfba3a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-10 23:46:07 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# https://www.virtualizor.com/docs/admin/adding-custom-control-panel/
 
+# check if virtualizor
+
+
+# clone script
 cd /tmp
 git clone https://github.com/stefanpejcic/OpenPanel-Virtualizor
 
+# setup according to https://www.virtualizor.com/docs/admin/adding-custom-control-panel/
 cd /tmp/OpenPanel-Virtualizor
 cp virt_openpanel.sh /usr/local/virtualizor/hooks/virt_openpanel.sh
 cp virt_openpanel.png /usr/local/virtualizor/enduser/themes/default/images/virt_openpanel.png
 cp openpanel_supported_os.json /usr/local/virtualizor/openpanel_supported_os.json
 
+#cleanul
 cd ../
 rm -rf /tmp/OpenPanel-Virtualizor
